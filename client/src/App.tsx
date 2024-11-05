208d5039c40bf8bc7199ee3a0026911fd3064271
janeeshafernando02 <janeeshafernando02@outlook.com>
2024-11-05 16:09:28 +0530

    feat(client): initial frontend project setup
    - react vite
    - tailwindcss
    - shadcn/ui

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="p-5 text-2xl text-red-900 font-bold">
+      <h1>Hirely AI</h1>
+      <Button className="mt-5 bg-green-800">Submit</Button>
+    </div>
+  );
+}
+
+export default App;
